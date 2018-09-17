32d7ba12dc475164ee343652983fffbdb30d18ca
Andrea <onofriandreapg@gmail.com>
2018-09-17 14:30:56 +0200

    Edited fitByGroup()

--- R/fitByGroup.R
+++ R/fitByGroup.R
@@ -17,10 +17,17 @@ fitByGroup <- function(Group, timeBef, timeAf, nSeeds) {
       tFirst <- dataTemp[dataTemp$timeBef==0,]$timeAf
 
       if(pMaxO < 0.1){
-          result <- rbind(result, c(i, pMaxO, NA, NA, pMaxO, Inf, rep(Inf, 18), rep(0, 18)))
-          next;}else{if(pFirst > 0.95){
-            result <- rbind(result, c(i, pMaxO, NA, NA, pMaxO, tFirst, rep(tFirst, 18), rep(1/tFirst, 18)))
-            next; } else{
+        #Pmax < 0.1
+        res <- c(i, nGerm, nTot, pMaxO, NA, NA, NA, Inf, rep(Inf, 18), rep(0, 18))
+
+        result <- rbind(result, res)
+        next;}else{if(pFirst > 0.95){
+
+          #Pmax at first inspection > 0.95
+          res <- c(i, nGerm, nTot, pMaxO, NA, NA, NA, tFirst, rep(tFirst, 18), rep(1/tFirst, 18))
+
+          result <- rbind(result, res)
+          next; } else{
             cureMod <- try( drm(nSeeds ~ timeBef + timeAf, data = dataTemp,
                           fct = LL.3(), type = "event",
                           upperl = c(NA, 1, NA)), silent=T )
@@ -29,8 +36,11 @@ fitByGroup <- function(Group, timeBef, timeAf, nSeeds) {
             } }
       class(cureMod); class(cureMod2)
       if(class(cureMod) == "try-error" & class(cureMod2) == "try-error"){
-        #print("caso1")
-        result <- rbind(result, c(i, pMaxO, NA, NA, pMaxO, NA, rep(NA, 18), rep(NA, 18)))
+
+        #No fit was possible
+        res <- c(i, nGerm, nTot, pMaxO, NA, NA, NA, NA, rep(NA, 18), rep(NA, 18))
+
+        result <- rbind(result, res)
         } else {if(class(cureMod) == "try-error" & class(cureMod2) == "drc"){
           coefs <- coef(cureMod2)
           RSS <- sum(residuals(cureMod2)^2)
@@ -42,7 +52,10 @@ fitByGroup <- function(Group, timeBef, timeAf, nSeeds) {
           tg1[is.na(tg1)] <- Inf
           GR1[is.na(GR1)] <- 0
           #GR1[,1]
-          res <- c(i, pMaxO, 2, coefs[1], 1, coefs[2], tg1[,1], tg2[,1], GR1[,1], GR2[,1])
+
+          #Fit with LL.2()
+          res <- c(i, nGerm, nTot, pMaxO, 2, coefs[1], 1, coefs[2], tg1[,1], tg2[,1], GR1[,1], GR2[,1])
+
           result <- rbind(result, res)
           #print("caso2")
           } else{ coefs <- coef(cureMod)
@@ -55,15 +68,18 @@ fitByGroup <- function(Group, timeBef, timeAf, nSeeds) {
           tg1[is.na(tg1)] <- Inf
           GR1[is.na(GR1)] <- 0
           GR1[,1]
-          res <- c(i, pMaxO, 3, coefs, tg1[,1], tg2[,1], GR1[,1], GR2[,1])
+
+          #Fit with LL.3()
+          res <- c(i, nGerm, nTot, pMaxO, 3, coefs, tg1[,1], tg2[,1], GR1[,1], GR2[,1])
+
           result <- rbind(result, res); #print("caso3")
           } }
     }
 
-  names(result) <- c("Group", "PmaxObs", "Model", "b", "d", "e", paste("GTA", seq(10,90, by=10), sep=""),
+  names(result) <- c("Group", "nGerm", "nTot", "PmaxObs", "Model", "b", "d", "e", paste("GTA", seq(10,90, by=10), sep=""),
                          paste("GTR", seq(10,90, by=10), sep=""),
                          paste("GRA", seq(10,90, by=10), sep=""),
                          paste("GRR", seq(10,90, by=10), sep=""))
-  result <- data.frame(Group=levels(Group), result[,2:42])
+  result <- data.frame(Group=levels(Group), result[,2:length(result[1,])])
   result
 }
