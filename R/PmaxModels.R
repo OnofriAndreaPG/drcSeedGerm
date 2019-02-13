4059996e1bae520cf32ff6a5a177ae3805dd7425
Andrea <onofriandreapg@gmail.com>
2019-02-13 08:10:21 +0100

    modified estfun.drc.r

--- R/PmaxModels.R
+++ R/PmaxModels.R
@@ -28,6 +28,50 @@ class(Pmax1) <- "drcMean"
 invisible(Pmax1)
 }
 
+# Psi <- seq(-1.5, 0, 0.1)
+# Pmax <- PmaxPsi1na.fun(Psi, -0.7, 0.2)
+# Pmax <-Pmax + rnorm(16, 0, 0.05)
+# Pmax <- ifelse(Pmax < 0, 0, Pmax)
+# Pmax <- ifelse(Pmax > 1, 0.99, Pmax)
+# Pmax
+#
+# mod <- drm(Pmax ~ Psi, fct = PmaxPsi1na())
+# summary(mod)
+#
+# mod <- drm(Pmax ~ Psi, fct = PmaxPsi1())
+# summary(mod)
+
+#Pmax vs Psi (shifted exponential, with asymptote)
+PmaxPsi1na.fun <- function(Psi, Psib, sigma) {
+  Pmax <- 1 - exp( - (Psi - Psib)/sigma)
+  Pmax <- ifelse(Pmax < 0 , 0, Pmax)
+  return(Pmax) }
+
+"PmaxPsi1na" <- function(){
+Pmax1na.fct <- function(x, parm) {
+  Pmax <- PmaxPsi1na.fun(x, parm[,1], parm[,2])
+  return(Pmax) }
+Pmax1na.names <- c("Psib", "sigma")
+Pmax1na.text <- "Shifted exponential distribution of base osmotic potential"
+Pmax1na.ss <- function(data){
+  data <- subset(data, data[,2]!=0)
+  x <- data[, 1]
+  y <- data[, 2]
+  G <- 1.05
+  pseudoY <- -log( (G - y)/G)
+  pseudoX <- x
+  coefs <- coef( lm(pseudoY ~ pseudoX) )
+  a <- coefs[1]
+  b <- coefs[2]
+  sigma <- 1/b
+  Psib <- - a * sigma
+  return(c(Psib, sigma))}
+Pmax1na <- list(fct = Pmax1na.fct, ssfct = Pmax1na.ss, names = Pmax1na.names, text = Pmax1na.text)
+class(Pmax1na) <- "drcMean"
+invisible(Pmax1na)
+}
+
+
 PmaxT1.fun <- function(Temp, G, Tc, sigmaTc) {
   Pmax <- G * (1 - exp( - (Tc - Temp) / sigmaTc))
   Pmax <- ifelse(Pmax < 0, 0, Pmax)
--- R/modified_sandwich.drc.R
+++ R/modified_sandwich.drc.R
@@ -1,5 +1,5 @@
 "estfun.drc" <- function (x, ...)
-#"estfun.drc" <- function (x, cvar = NULL, ...)  
+#"estfun.drc" <- function (x, cvar = NULL, ...)
 {
     ## Extending the matrix of derivatives to have one column per parameter (not only per model parameter)
     ##  only relevant in case several curves (with different parameters) were fitted
@@ -29,9 +29,9 @@
     ## One column per parameter, with 0s
     xderiv2Fct <- function(xderiv1, indMat, curveID)
     {
-        xderiv2 <- xderiv1[, rep(1:ncol(xderiv1), apply(indMat, 1, function(x){length(unique(x))}))]    
+        xderiv2 <- xderiv1[, rep(1:ncol(xderiv1), apply(indMat, 1, function(x){length(unique(x))}))]
 #        print(xderiv2)
-        
+
         cnInd <- colnames(indMat)
         for (i in 1:ncol(indMat))
         {
@@ -42,11 +42,11 @@
 
     if (identical(x$type, "continuous"))
     {
-        xderiv2 <- xderiv2Fct(xderiv1, indMat, curveID)    
+        xderiv2 <- xderiv2Fct(xderiv1, indMat, curveID)
 #        rval <- (weights(x) * residuals(x)) * x$deriv1
         rval <- (weights(x) * residuals(x)) * xderiv2
     }
-    
+
     if (identical(x$type, "binomial"))
     {  # not handling missing values
         nTotal <- weights(x)
@@ -56,7 +56,7 @@
         rval0[!is.finite(rval0)] <- 0  # handling fitted values equal to 0 or 1
 #        rval <- x$deriv1 * rval0
         xderiv2 <- xderiv2Fct(xderiv1, indMat, curveID)
-        rval <- xderiv2 * rval0    
+        rval <- xderiv2 * rval0
         rval
     }
     if (identical(x$type, "Poisson"))
@@ -65,13 +65,13 @@
         fittedVal <- fitted(x)
         rval0 <- resp / fittedVal - 1
 #        rval <- x$deriv1 * rval0
-        xderiv2 <- xderiv2Fct(xderiv1, indMat, curveID)  
-        rval <- xderiv2 * rval0  
+        xderiv2 <- xderiv2Fct(xderiv1, indMat, curveID)
+        rval <- xderiv2 * rval0
         rval
-    }    
+    }
     if (identical(x$type, "event"))
     {  # not handling missing values
-       
+
         doses <- NULL
         if(is.vector(x$dataList$dose)==F) doses <- 3:(length(x$dataList[["dose"]][1,])+1)
         #pr <- x[["fct"]]$"deriv1"(x$data[, c(1, doses)], t(x[["parmMat"]][, as.character(x$data[, 5])]))
@@ -81,21 +81,27 @@
         countCol <- ifelse(is.null(doses), 3, 3 + length(x$dataList[["dose"]][1,]) - 1)
         Ft1 <- predict(x, data.frame(x$data[, c(1, doses)], x$data[, ids]))
         Ft2 <- predict(x, data.frame(x$data[, c(2, doses)], x$data[, ids]))
-        Ft2[!is.finite(x$data[, 2])] <- 1    
+        Ft2[!is.finite(x$data[, 2])] <- 1
         diffF <- Ft2 - Ft1 #likelihood for individual seed
         diffF[diffF==0] <- 10e-6
-        DFt1.0 <- x[["fct"]]$"deriv1"(x$data[, c(1, doses)], t(x[["parmMat"]][, as.character(x$data[, ids])]))        
-        DFt2.0 <- x[["fct"]]$"deriv1"(x$data[, c(2, doses)], t(x[["parmMat"]][, as.character(x$data[, ids])]))
-        
-        DFt2.0[!is.finite(x$data[, 2]), ] <- 0         
+        #Corrected on 12/2/19
+        #DFt1.0 <- x[["fct"]]$"deriv1"(x$data[, c(1, doses)], t(x[["parmMat"]][, as.character(x$data[, ids])]))
+        #DFt2.0 <- x[["fct"]]$"deriv1"(x$data[, c(2, doses)], t(x[["parmMat"]][, as.character(x$data[, ids])]))
+
+        DFt1.0 <- x[["fct"]]$"deriv1"(x$data[, c(1, doses)], t(x[["parmMat"]][, as.character(x$data[, ids + 1])]))
+        DFt2.0 <- x[["fct"]]$"deriv1"(x$data[, c(2, doses)], t(x[["parmMat"]][, as.character(x$data[, ids + 1])]))
+
+        DFt2.0[!is.finite(x$data[, 2]), ] <- 0
         diffDF.0 <- DFt2.0 - DFt1.0 #Derivative of likelihood funct
-        
-        diffDF <- xderiv2Fct(diffDF.0, indMat, x$data[, ids])
-        
-        rval <- (x$data[, countCol] / diffF) * diffDF #Derivative composite function D(log(f(x))) 
+
+        #Modified on 12/2/19
+        #diffDF <- xderiv2Fct(diffDF.0, indMat, x$data[, ids + 1])
+        diffDF <- xderiv2Fct(diffDF.0, indMat, x$data[, ids + 1])
+        #print(diffDF); stop()
+        rval <- (x$data[, countCol] / diffF) * diffDF #Derivative composite function D(log(f(x)))
         #print(diffDF)
         if (FALSE)
-        { 
+        {
         dataList <- x[["data"]]
         xderiv1 <- x[["fct"]]$"deriv1"(dataList[, 1], t(x[["parmMat"]][, as.character(dataList[, (ids - 1)])]))
         #print(xderiv1)
@@ -112,7 +118,7 @@
 ##        lagDeriv1 <- apply(x$deriv1, 2, function(x){diff(x)})
 #        lagDeriv1 <- apply(xderiv2, 2, function(x){diff(x)})
 #        rval <- lagDeriv1 * rval0
-#        rval <- xderiv2 * rval0    
+#        rval <- xderiv2 * rval0
 #        rval
         rval <- xderiv2 * (diff(resp) / diff(fittedVal))
         }
@@ -121,15 +127,15 @@
 #    if (!is.null(cvar))
 #    {
 #        rval <- rowsum(rval, cvar)
-#    }    
-        
+#    }
+
 #    print(rval)
     colnames(rval) <- names(coef(x))
-#    colnames(rval) <- x$fct$names       
+#    colnames(rval) <- x$fct$names
     rval
 }
 
-"bread.drc" <- function (x, ...) 
+"bread.drc" <- function (x, ...)
 {
 #    if (identical(x$type, "binomial"))
 #    {
