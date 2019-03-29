828e0ebd7e9e7ad3177799c0eb6ac1348172d259
Andrea Onofri <OnofriAndreaPG@gmail.com>
2019-03-29 10:57:23 +0100

    Added derivatives to GR models

--- R/GRModels.R
+++ R/GRModels.R
@@ -199,10 +199,39 @@ ss <- function(data){
   k <- ss2[2]
   To <- - ss2[1] / k
   Tc <- (1 - ss2[1])/ss2[2]
+return(c(Tb, ThetaT, k, Tc))}
 
-  return(c(Tb, ThetaT, k, Tc))}
+deriv1 <- function(x, parm){
+  #Approximation by using finite differences
+  d1.1 <- GRT.Exb.fun(x, parm[,1], parm[,2], parm[,3],
+                   parm[,4])
+  d1.2 <- GRT.Exb.fun(x, (parm[,1] + 10e-6), parm[,2], parm[,3],
+                   parm[,4])
+  d1 <- (d1.2 - d1.1)/10e-6
+
+  d2.1 <- GRT.Exb.fun(x, parm[,1], parm[,2], parm[,3],
+                   parm[,4])
+  d2.2 <- GRT.Exb.fun(x, parm[,1], (parm[,2] + 10e-6), parm[,3],
+                   parm[,4])
+  d2 <- (d2.2 - d2.1)/10e-6
+
+  d3.1 <- GRT.Exb.fun(x, parm[,1], parm[,2], parm[,3],
+                   parm[,4])
+  d3.2 <- GRT.Exb.fun(x, parm[,1], parm[,2], (parm[,3] + 10e-6),
+                   parm[,4])
+  d3 <- (d3.2 - d3.1)/10e-6
+
+  d4.1 <- GRT.Exb.fun(x, parm[,1], parm[,2], parm[,3],
+                   parm[,4])
+  d4.2 <- GRT.Exb.fun(x, parm[,1], parm[,2], parm[,3],
+                   (parm[,4] + 10e-6))
+  d4 <- (d4.2 - d4.1)/10e-6
+
+  cbind(d1, d2, d3, d4)
+}
+
 text <- "Exponential effect of temperature on GR50 (Type II - Masin et al., 2017)"
-returnList <- list(fct=fct, ssfct=ss, names=names, text=text)
+returnList <- list(fct=fct, ssfct=ss, names=names, text=text, deriv1 = deriv1)
 class(returnList) <- "drcMean"
 invisible(returnList)
 }
